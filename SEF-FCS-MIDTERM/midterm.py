9e43e61d355b3e37c6037b4909ca8078eb1fd255
Zein Shhadi <zeinshhadi@hotmail.com>
2023-07-29 21:25:27 +0300

    removed binary search

--- SEF-FCS-MIDTERM/midterm.py
+++ SEF-FCS-MIDTERM/midterm.py
@@ -90,30 +90,6 @@ def merge(left, right,option):
     
     return merged_list
             
-###### - BS Algorithm - ######
-
-def binary_search(arr, low, high, x):
- 
-
-    if high >= low:
- 
-        mid = (high + low) // 2
- 
-        if arr[mid] == x:
-            return mid
-
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
-
-        else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-   
-        return 'ID not found in list'
-
-
-
 ####### - Upload tickets and events to corresponding lists and dictionaries - ########
 def UploadTickets( tickets, events):
     with open('events_data.txt', 'r') as file:
